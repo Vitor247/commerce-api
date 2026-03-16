78c60385edc688bd8bd88764cf50ed8aaa9afb09
Vitor Camilo <vittor.camilo@gmail.com>
2026-03-16 18:39:39 -0300

    project created

---
+++ src/test/java/com/vitorcamilodev/commerce/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorcamilodev.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
