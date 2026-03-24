947ef045745ce4c27a7eae3a5b654012a8b54123
Vitor Camilo <vittor.camilo@gmail.com>
2026-03-24 17:33:53 -0300

    insert product

--- src/main/java/com/vitorcamilodev/commerce/controllers/ProductController.java
+++ src/main/java/com/vitorcamilodev/commerce/controllers/ProductController.java
@@ -1,19 +1,26 @@
 package com.vitorcamilodev.commerce.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitorcamilodev.commerce.dto.ProductDTO;
 import com.vitorcamilodev.commerce.dto.ProductMinDTO;
 import com.vitorcamilodev.commerce.entities.PageResponse;
 import com.vitorcamilodev.commerce.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -36,4 +43,10 @@ public class ProductController {
 		return ResponseEntity.ok(new PageResponse<>(page));
 	}
 
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
--- src/main/java/com/vitorcamilodev/commerce/services/ProductService.java
+++ src/main/java/com/vitorcamilodev/commerce/services/ProductService.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.vitorcamilodev.commerce.dto.CategoryDTO;
 import com.vitorcamilodev.commerce.dto.ProductDTO;
 import com.vitorcamilodev.commerce.dto.ProductMinDTO;
+import com.vitorcamilodev.commerce.entities.Category;
 import com.vitorcamilodev.commerce.entities.Product;
 import com.vitorcamilodev.commerce.repositories.ProductRepository;
 import com.vitorcamilodev.commerce.services.exceptions.ResourceNotFoundException;
@@ -31,4 +33,25 @@ public class ProductService {
 		return result.map(x -> new ProductMinDTO(x));
 	}
 	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category cat = new Category();
+			cat.setId(catDto.getId());
+			entity.getCategories().add(cat);
+		}
+	}
 }
