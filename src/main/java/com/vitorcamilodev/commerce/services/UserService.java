4ff92fbe4cbdf5e30ec8f17ac020f4acedc6519e
Vitor Camilo <vittor.camilo@gmail.com>
2026-03-24 20:45:44 -0300

    userService, searchByEmail

---
+++ src/main/java/com/vitorcamilodev/commerce/services/UserService.java
@@ -0,0 +1,38 @@
+package com.vitorcamilodev.commerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vitorcamilodev.commerce.entities.Role;
+import com.vitorcamilodev.commerce.entities.User;
+import com.vitorcamilodev.commerce.projections.UserDetailsProjection;
+import com.vitorcamilodev.commerce.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if (result.size() == 0) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+
+		return user;
+	}
+
+}
