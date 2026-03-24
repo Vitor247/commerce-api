558151da51ddc4145a421c6168ffc801a004b21e
Vitor Camilo <vittor.camilo@gmail.com>
2026-03-24 17:42:57 -0300

    delete product

--- src/main/java/com/vitorcamilodev/commerce/services/ProductService.java
+++ src/main/java/com/vitorcamilodev/commerce/services/ProductService.java
@@ -1,9 +1,11 @@
 package com.vitorcamilodev.commerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vitorcamilodev.commerce.dto.CategoryDTO;
@@ -12,6 +14,7 @@ import com.vitorcamilodev.commerce.dto.ProductMinDTO;
 import com.vitorcamilodev.commerce.entities.Category;
 import com.vitorcamilodev.commerce.entities.Product;
 import com.vitorcamilodev.commerce.repositories.ProductRepository;
+import com.vitorcamilodev.commerce.services.exceptions.DatabaseException;
 import com.vitorcamilodev.commerce.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -52,6 +55,18 @@ public class ProductService {
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Falha de integridade referencial");
 		}
 	}
 	
