2170f3887da4a424e7e11aa1b60c790b33e34b3d
Vitor Camilo <vittor.camilo@gmail.com>
2026-03-25 20:05:17 -0300

    validate admin

--- src/main/java/com/vitorcamilodev/commerce/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/vitorcamilodev/commerce/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,6 @@
 package com.vitorcamilodev.commerce.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(String msg) {
